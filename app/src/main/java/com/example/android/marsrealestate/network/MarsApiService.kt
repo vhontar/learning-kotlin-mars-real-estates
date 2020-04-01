9d8644a5163a39e8c3a01cc397791a8c10ac5003
Vladyslav Hontar <vlad.hontar@techs.com.ua>
2020-04-02 01:27:15 +0300

    finished project with using moshi, glide, retrofit, MVVM + livedata and transformations, BindAdapters;

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -19,7 +19,6 @@ package com.example.android.marsrealestate.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
